c36f85ef6b52ebf11639134c3e2a2a777727fd01
NeilRow56 <neilrowland56@gmail.com>
2024-10-27 14:07:46 +0000

    Created function foe getting products on Dashboard page. Created EmptyState component

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,10 +1,28 @@
+
+import { EmptyState } from '@/components/dashboard/EmptyState'
+import { getProducts } from '@/server/db/product'
+import { RedirectToSignIn } from '@clerk/nextjs'
+import { auth } from '@clerk/nextjs/server'
 import React from 'react'
 
 export const metadata = {
   title: "Dashboard",
 }
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+ const { userId} = await auth()
+
+ if(userId == null) return <RedirectToSignIn />
+  const products = await getProducts(userId, {limit: 6})
+
+  if(products.length === 0) return <EmptyState
+  title="You dont have any Products created"
+            description="You currently dont have any Products. Once created you can
+      see them here!"
+            buttonText="Create Product"
+            href="/products/newProduct"
+  
+  />
   return (
     <div>Dashboard Page</div>
   )
---
+++ src/server/db/product.ts
@@ -0,0 +1,13 @@
+import { db } from "@/drizzle/db";
+
+
+
+export function getProducts(
+    userId: string, {limit}: {limit?: number}
+  )  {
+    return db.query.ProductTable.findMany({
+        where: (({clerkUserId}, {eq}) => eq(clerkUserId, userId)),
+        orderBy: (({createdAt}, {desc}) => desc(createdAt)),
+        limit
+    })
+  }
