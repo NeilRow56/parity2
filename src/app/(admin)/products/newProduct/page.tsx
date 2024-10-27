aa15096bce2e5ab1a0ca6f29b5a1003f0b48d322
NeilRow56 <neilrowland56@gmail.com>
2024-10-27 16:10:25 +0000

    Added PageWithBackButton component to newProduct page

--- src/app/(admin)/products/newProduct/page.tsx
+++ src/app/(admin)/products/newProduct/page.tsx
@@ -1,7 +1,26 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+
+import { PageWithBackButton } from '@/components/dashboard/PagewithBackButton'
 import React from 'react'
 
 export default function CreateProductPage() {
   return (
-    <div>Create Product Page</div>
+    <div>
+     <PageWithBackButton
+      pageTitle="Create Product"
+      backButtonHref="/products"
+    >
+     
+        <Card>
+          <CardHeader>
+            <CardTitle className="text-xl">Product Details</CardTitle>
+          </CardHeader>
+          <CardContent>
+           PRODUCT DETAILS FORM
+          </CardContent>
+        </Card>
+      
+    </PageWithBackButton>
+    </div>
   )
 }
