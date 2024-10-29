2ae0c1400a582b9af1a11d421e9ba41ff047e4e1
NeilRow56 <neilrowland56@gmail.com>
2024-10-29 06:58:05 +0000

    Completed tagging and revalidation for subscriptions and users

--- src/server/db/subscription.ts
+++ src/server/db/subscription.ts
@@ -1,13 +1,28 @@
 import { db } from "@/drizzle/db"
 import { UserSubscriptionTable } from "@/drizzle/schema"
+import { CACHE_TAGS, revalidateDbCache } from "@/lib/cache"
 
 export async function createUserSubscription(
-    data: typeof UserSubscriptionTable.$inferInsert
-  ) {
+  data: typeof UserSubscriptionTable.$inferInsert
+) {
+  const [newSubscription] = await db
+    .insert(UserSubscriptionTable)
+    .values(data)
+    .onConflictDoNothing({
+      target: UserSubscriptionTable.clerkUserId,
+    })
+    .returning({
+      id: UserSubscriptionTable.id,
+      userId: UserSubscriptionTable.clerkUserId,
+    })
 
-    return db.insert(UserSubscriptionTable).values(data)
-   // To avoid duplicating a user 
-      .onConflictDoNothing({
-        target: UserSubscriptionTable.clerkUserId,
-      })   
+  if (newSubscription != null) {
+    revalidateDbCache({
+      tag: CACHE_TAGS.subscription,
+      id: newSubscription.id,
+      userId: newSubscription.userId,
+    })
   }
+
+  return newSubscription
+}
